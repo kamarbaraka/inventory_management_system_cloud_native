778b923c37de4eaaee77e5264b80274b45b2066e
kamarbaraka <kamar254baraka@gmail.com>
2023-08-20 18:34:39 +0300

    customer entity change in the main branch.
    added a customer username and password field.
    
    Signed-off-by: kamarbaraka <kamar254baraka@gmail.com>

--- src/main/java/com/kamar/inventory_management_system_cloud_native/components/persistence/entities/Customer.java
+++ src/main/java/com/kamar/inventory_management_system_cloud_native/components/persistence/entities/Customer.java
@@ -1,30 +1,50 @@
 package com.kamar.inventory_management_system_cloud_native.components.persistence.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 
 /**
  * a customer entity.
  * @author kamar baraka.*/
 
 @Entity
+@Table(
+        uniqueConstraints = @UniqueConstraint(columnNames = {"customer_username", })
+)
 public class Customer {
 
     @Id
     @GeneratedValue
     private long customerId;
 
+    private String customerUsername;
+
     private String customerName;
 
     private String customerContact;
+
+    private String password;
 
     @ManyToOne
     private Address address;
 
     public long getCustomerId() {
         return customerId;
+    }
+
+    public String getCustomerUsername() {
+        return customerUsername;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setCustomerUsername(String customerUsername) {
+        this.customerUsername = customerUsername;
     }
 
     public String getCustomerName() {
