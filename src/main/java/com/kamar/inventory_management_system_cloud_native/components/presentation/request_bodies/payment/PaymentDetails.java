bc7e5a393117e95545155da3384d31f360e24866
kamarbaraka <kamar254baraka@gmail.com>
2023-08-27 12:47:49 +0300

    fine-tuned ordering system. for #12
    Signed-off-by: kamarbaraka <kamar254baraka@gmail.com>

---
+++ src/main/java/com/kamar/inventory_management_system_cloud_native/components/presentation/request_bodies/payment/PaymentDetails.java
@@ -0,0 +1,55 @@
+package com.kamar.inventory_management_system_cloud_native.components.presentation.request_bodies.payment;
+
+/**
+ * payment details request body.
+ * @author kamar baraka.*/
+
+public class PaymentDetails {
+
+    private long orderId;
+
+    private String paymentType;
+    private String bankName;
+    private String accountName;
+    private String mobileNumber;
+
+    public long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(long orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getPaymentType() {
+        return paymentType;
+    }
+
+    public void setPaymentType(String paymentType) {
+        this.paymentType = paymentType;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+}
